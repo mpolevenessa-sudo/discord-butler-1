a16f7684090e3b0829e47cb4403fdb5d77b2c0b2
mp0l3 <mpolevenessa@gmail.com>
2026-05-18 17:45:01 +0300

    Update groq_agent.py

--- groq_agent.py
+++ groq_agent.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 import database as db
 
 load_dotenv(override=True)
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+api_key = os.getenv("GROQ_API_KEY")
+client = Groq(api_key=api_key)
 
 def process_user_message(discord_id, user_message, server_name="this community"):
     # Guardrails
